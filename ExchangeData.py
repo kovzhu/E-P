132f4c035367f5efa81b789c3112c69ed1e6a1d1
Kov <44253207+kovzhu@users.noreply.github.com>
2021-01-14 20:06:30 +0800

    Update ExchangeData.py

--- ExchangeData.py
+++ ExchangeData.py
@@ -3,6 +3,8 @@ import requests
 import json
 import pandas as pd
 import sys
+from bs4 import BeautifulSoup as bs
+from tools import WriteToExcel
 
 def gettime():
     return int(round(time.time()*1000))
@@ -44,7 +46,7 @@ def getResponse(url, headers, params):
         sys.exit()
 
 
-def get_data(pages):
+def get_Shanghai_data(pages):
     headers = {
         'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
         'Referer': r'https://www.shpgx.com/html/gdtrqsj.html',
@@ -54,7 +56,8 @@ def get_data(pages):
     table = pd.DataFrame()
     for i in range(pages):
         params =    {
-            'wareid': 6,
+            # 'wareid': 6, # for pipeline deals
+            'wareid': 3, # for LNG deals
             'cd':None,
             'starttime':None,
             'endtime':None,
@@ -88,3 +91,43 @@ def get_data(pages):
                 '挂牌日期' : data.iloc[i]['orderdate']}, orient = 'index')
             table = pd.merge(table, df, how='outer', left_index=True, right_index=True)
     return table.T
+
+
+def get_Chongqing_data(start_date = '20180101', end_date ='20201206', pages = 749):
+
+    headers = {
+        'User-Agent': r'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+        'Referer': r'https://www.chinacqpgx.com/jyxx/index.php?type=0&area=&t1=20201201&t2=20201206',
+        'Connection': r'keep-alive',
+        'Cookie':r'Cookie: zero_areaArr3=%5B%22%E5%9B%9B%E5%B7%9D%22%2C%22%E5%8C%97%E4%BA%AC%22%2C%22%E9%87%8D%E5%BA%86%22%2C%22%E5%AE%81%E5%A4%8F%22%2C%22%E5%B9%BF%E5%B7%9E%22%2C%22%E9%9D%92%E6%B5%B7%22%2C%22%E5%8D%97%E5%AE%81%22%2C%22%E4%B8%8A%E6%B5%B7%22%2C%22%E5%86%85%E8%92%99%E5%8F%A4%22%2C%22%E4%BA%91%E5%8D%97%22%2C%22%E7%94%98%E8%82%83%22%2C%22%E6%96%B0%E7%96%86%22%2C%22%E9%99%95%E8%A5%BF%22%2C%22%E6%B9%96%E5%8D%97%22%2C%22%E8%B4%B5%E5%B7%9E%22%2C%22%E6%B1%9F%E8%A5%BF%22%2C%22%E6%B9%96%E5%8C%97%22%2C%22%E5%B9%BF%E8%A5%BF%22%2C%22%E5%90%89%E6%9E%97%22%2C%22%E8%BE%BD%E5%AE%81%22%2C%22%E5%A4%A9%E6%B4%A5%22%2C%22%E5%AE%89%E5%BE%BD%22%2C%22%E5%B9%BF%E4%B8%9C%22%2C%22%E5%B1%B1%E8%A5%BF%22%2C%22%E6%B1%9F%E8%8B%8F%22%2C%22%E6%B2%B3%E5%8C%97%22%2C%22%E5%B1%B1%E4%B8%9C%22%2C%22%E6%B2%B3%E5%8D%97%22%2C%22%E7%A6%8F%E5%BB%BA%22%2C%22%E5%85%A8%E4%B8%AD%E5%9B%BD%22%5D; Hm_lvt_f81598e2508cd3d34d620f5689165095=1610620639; Hm_lpvt_f81598e2508cd3d34d620f5689165095=1610620793'}
+    page_data = pd.DataFrame()
+    for i in range(1,pages+1):
+        params = {
+            'type':'all',
+            'area':None,
+            't1': start_date,
+            't2': end_date,
+            'p':i
+        }
+        url = r'https://www.chinacqpgx.com/jyxx/index.php'
+        s = requests.session()
+        table = pd.DataFrame()
+        # response = s.get(url, headers=headers, params=params).text.encode('utf-8')
+        response = s.get(url, headers=headers, params=params)
+        soup = bs(response.text, features='lxml')
+        code = soup.find_all(name='div', attrs={'class': 'trade-content'})[0].find_all('li')
+        rows = len (code)-6
+
+        for i in range(rows):
+            row_data = pd.DataFrame.from_dict({
+                code[0].text:code[6+i].text.splitlines()[2],
+                code[1].text : code[6+i].text.splitlines()[3],
+                code[2].text : code[6+i].text.splitlines()[4],
+                code[3].text : code[6+i].text.splitlines()[5],
+                code[4].text : code[6+i].text.splitlines()[6],
+                code[5].text : code[6+i].text.splitlines()[7]
+
+            }, orient='index')
+            page_data = pd.merge(page_data,row_data,how='outer',left_index=True, right_index=True)
+    WriteToExcel('Chongqing data 20200114.xlsx','data', page_data.T)
+    return page_data.T
